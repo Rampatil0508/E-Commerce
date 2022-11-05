cf7da61911f03235bea1f3397d02b5bae89ed1dd
Rampatil0508 <117507729+Rampatil0508@users.noreply.github.com>
2022-11-05 18:19:46 +0530

    kalpesh bhau

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,11 @@
 
 public class LinkedList {kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-
+ 
+  public class cirq {
+    
+  public static void main(String [] args)
+  {
+    system.out.println("jay shree ram");
+  }
+  }
 }
